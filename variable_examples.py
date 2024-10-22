119df9d5f494c15c3f4edef3535d2e24a25c6e9c
Mohammad Hadi <mhadi1@appdev.yearup.org>
2024-10-22 12:31:09 -0500

    Lab No 1

---
+++ variable_examples.py
@@ -0,0 +1,28 @@
+
+
+CustomerID = 4546
+
+CustomerName = 'omar' 
+middle = 'bin'
+lastName = 'AbdulAziz' 
+
+Gender = 'Male'
+
+DOB = 11/2/61
+
+Driverslicensenumber = 51564156
+
+Autopolicynumber = 15162
+
+# for words which have word i put them between '' and for numbers i didnt put anything.
+
+
+Name = 'Mohammad'
+
+Cityofbirth='Chicago'
+
+
+
+
+
+
