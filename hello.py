777783f93ee4d7489eebfe62a97ecc5df005ba1e
Mohammad Hadi <mhadi1@appdev.yearup.org>
2024-10-22 08:45:51 -0500

    lab 1

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("hello world")
+print('hello world')
+
+
+message = 'hello world'
+print(message)
+
+dollar = 3
+cents =0.50
+
+print(dollar+cents)
+
+
+print('3 dollars','fifty cents')
+
+
+
+
