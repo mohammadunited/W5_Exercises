2211ebd6bb625266927d2e0349b818ac97fa8fa1
Mohammad Hadi <mhadi1@appdev.yearup.org>
2024-10-22 14:43:29 -0500

    Lab 2

--- variable_examples.py
+++ variable_examples.py
@@ -21,6 +21,14 @@ Name = 'Mohammad'
 
 Cityofbirth='Chicago'
 
+#False	def	if	raise None	del	import	return True	elif	in	try and	else	is	while as	except lambda with assert finally nonlocal yield break for not class form or continue global	pass
+
+# False is a boolean operator that represents the opposite of true, if is An if statement is used to make a conditional statement.
+# If the condition is True, then some action is performed.
+# ^^elis is Shorthand for else if, checks if some other condition holds when the condition in the if statement is false.
+# ^^in is The in statement is used to check if an element is present in an iterable like list or tuple.
+# ^^The try statement is used to make a try… except statement. The try statement starts a block of code that is tried to execute. If it fails, the except block catches the error.
+
 
 
 
