b7c39185f684cead623da9cd229df61edec06756
Mohammad Hadi <mhadi1@appdev.yearup.org>
2024-10-24 09:16:40 -0500

    f_to_c.py

---
+++ MathScripts/f_to_c.py
@@ -0,0 +1,13 @@
+
+
+fahrenheight = 65
+subraction = 32
+
+multiply1 = 5
+multiply2 = 9
+result = multiply1/multiply2
+
+celsius = (fahrenheight - subraction) * result
+
+print("When the farenheight is",fahrenheight,"the celsius will be",celsius,)
+
