0cf7bdfc1e16a983350afe755d6ec485d9616502
Mohammad Hadi <mhadi1@appdev.yearup.org>
2024-10-29 15:37:53 -0500

    3.1

---
+++ MathScripts/NumericConversions/conversion_tests.py
@@ -0,0 +1,14 @@
+
+# Description: This script tests various numeric
+# conversion techniques
+# Author: Mohammad. Newprogrammer
+
+a = "  101.1"
+int=(float(a))
+a1=a.strip()
+print(a1)
+int  = (55)
+c = "402 Stevens"
+d = 'Number 5 '
+d.strip('')
+print(d)
---
+++ MathScripts/tiles.py
@@ -0,0 +1,13 @@
+
+import math
+
+
+lenght = 28
+
+width = 55
+
+roomL = width + lenght
+
+tilesbox = 12
+
+eachtile= 1 by one
