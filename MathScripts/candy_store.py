74aa8c26ada3c324c2dadd45b67d1d9e785ac798
Mohammad Hadi <mhadi1@appdev.yearup.org>
2024-10-31 14:21:55 -0500

    lab2

--- MathScripts/candy_store.py
+++ MathScripts/candy_store.py
@@ -1,3 +1,11 @@
 
-candle_companies = ("fruit loops","Gushers","Air heads")
+candy_companies = ("fruit loops","Gushers","Air heads")
 
+fruit_flavours = ("watermelon","Orange","Blueberyy")
+
+combination_today = candy_companies, fruit_flavours
+{fruit_flavours[1]+fruit_flavours[2]+candy_companies[2]+candy_companies[1]}
+
+
+
+print("Today’s candy options include:",fruit_flavours[0]+candy_companies[2])
